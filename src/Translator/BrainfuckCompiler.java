90552c870267dd9f9cd59793bb8b5093c7db4b48
Tornio2 <ntornev@gmail.com>
2025-10-08 15:01:26 +0300

    modified it so it reads program from a file and also outputs it

--- src/Translator/BrainfuckCompiler.java
+++ src/Translator/BrainfuckCompiler.java
@@ -1,27 +1,33 @@
 package Translator;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class BrainfuckCompiler {
     public static void main(String[] args) {
-        String sourceCode = "VAR x = 5\n" +
-                "VAR y = 3\n" +
-                "VAR sum = x + y\n" +
-                "PRINT y\n"
-//                +
-//                "WHILE sum > 0\n" +
-//                "  PRINT sum\n" +
-//                "  sum = sum - 1\n" +
-//                "END"
-                ;
+        try {
+            String sourceCode = new String(Files.readAllBytes(Paths.get("src/Translator/program.txt")));
 
-        Lexer lexer = new Lexer(sourceCode);
-        Parser parser = new Parser(lexer);
-        CodeGenerator generator = new CodeGenerator();
+            Lexer lexer = new Lexer(sourceCode);
+            Parser parser = new Parser(lexer);
 
-        AST ast = parser.parse();
-        String brainfuckCode = generator.generate(ast);
+            String brainfuckCode = parser.parse();
 
-        System.out.println("Generated Brainfuck code:");
-        System.out.println(brainfuckCode);
+            // Write the Brainfuck code to a file
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.bf"));
+            writer.write(brainfuckCode);
+            writer.close();
+
+            System.out.println("Compilation successful. Brainfuck code written to output.bf");
+            System.out.println("Generated Brainfuck code:");
+            System.out.println(brainfuckCode);
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
